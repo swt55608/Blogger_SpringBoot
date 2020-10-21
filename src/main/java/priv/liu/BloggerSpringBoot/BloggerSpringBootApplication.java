37d575aba598c30145b91882146a1946030596f7
106598047 <1101105310@gm.kuas.edu.tw>
2020-10-21 16:15:50 +0800

    Initial Commit

---
+++ src/main/java/priv/liu/BloggerSpringBoot/BloggerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package priv.liu.BloggerSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/priv/liu/BloggerSpringBoot/BloggerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package priv.liu.BloggerSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
